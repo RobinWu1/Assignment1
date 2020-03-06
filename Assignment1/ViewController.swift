4839e2870af562ee7d1d08eb1b30995b38282a3a
RobinWu1 <wuyilong182@126.com>
2020-03-06 14:42:49 -0400

    Initial Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by RobinWu on 2020-03-06.
+//  Copyright © 2020 RobinWu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
